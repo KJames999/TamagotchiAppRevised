cf9068190ce281e8243f996361ebcdd792ba2b74
Killian James <james.k@etoncollege.org.uk>
2022-01-20 11:49:03 +0000

    Initial Commit

---
+++ TamagotchiApp/TamagotchiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiAppApp.swift
+//  TamagotchiApp
+//
+//  Created by James, Killian (NA) on 20/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
